a303a60c20d81fe503bf2369fdd69805b2f915e5
Rahib M. Cafarov <CafarovR@kapitalho.az>
2021-05-19 16:44:57 +0400

    first commit

---
+++ TestAPI/Controllers/LoginController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TestAPI.Entities;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string username, string pass)
+        {
+            UserModel login = new UserModel();
+            login.UserName = username;
+            login.Password = pass;
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenStr = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+
+            return response;
+        }
+
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+            var userName = claim[0].Value;
+            return "Wellcome To: " + userName;
+        }
+
+        [Authorize]
+        [HttpGet("GetValue")]
+        public IActionResult Get()
+        {
+            var str = new string[] { "v1", "v2", "v3" };
+            return Ok(str);
+        }
+
+        private string GenerateJSONWebToken(UserModel userinfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub , userinfo.UserName),
+                new Claim(JwtRegisteredClaimNames.Email , userinfo.EmailAdress),
+                new Claim(JwtRegisteredClaimNames.Jti , Guid.NewGuid().ToString())
+
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"], 
+                audience: _config["Jwt:Issuer"], 
+                claims, 
+                expires: DateTime.Now.AddMinutes(120), 
+                signingCredentials: credentials
+                );
+
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return encodetoken;
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+            if (login.UserName == "rahib" && login.Password == "123")
+            {
+                user = new UserModel
+                {
+                    Password = "123",
+                    UserName = "rahib",
+                    EmailAdress = "rahib@gmail.com"
+                };
+            }
+
+            return user;
+        }
+    }
+}
---
+++ TestAPI/Controllers/TestController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = new string[]
+            {
+                "Rahib", "Rahib2", "Rahib3", "Rahib4"
+            };
+            return Ok(data);
+        }
+
+        Random rnd;
+        List<int> arry;
+
+        [HttpPost("a")]
+        public IActionResult Get(int a)
+        {
+            if (arry == null)
+            {
+                arry = new List<int>();
+            }
+            else
+            {
+                arry.Clear();
+            }
+            if (rnd is null)
+            {
+                rnd = new Random();
+            }
+            if (a>=3)
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    int b = rnd.Next(0, 100);
+                    arry.Add(b);
+                }
+                return Ok(arry);
+            }
+            return BadRequest("3 den boyuk olmalidir.");  
+        }
+    }
+}
